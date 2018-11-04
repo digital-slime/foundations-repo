b77f3b1ddb5d453269af2e518ef2b4cd650b8508
digital-slime <izeilinger2@gmail.com>
2018-11-04 16:07:13 -0600

    added stat roll system

---
+++ app/src/main/java/com/example/zeilinger/test/Main2Activity.java
@@ -0,0 +1,91 @@
+package com.example.zeilinger.test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private TextView character_point_text_output;
+    private int point_total;
+    private int STR, DEX, CON, INT, WIS, CHA;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    Intent myintent = new Intent(Main2Activity.this,MainActivity.class);
+                    startActivity(myintent);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_points);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    //refresh instance
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        point_total = 27;
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        character_point_text_output = (TextView) findViewById(R.id.character_point_text);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mTextMessage.setText(getString(R.string.point_buy_counter,"27"));
+        character_point_text_output.setText(getString(R.string.point_buy_output,
+                Integer.toString(STR),Integer.toString(DEX),Integer.toString(CON),
+                Integer.toString(INT),Integer.toString(WIS),Integer.toString(CHA)));
+    }
+
+
+
+    public void rollstats(View view) {
+        mTextMessage.setText("points unused");
+        character_point_text_output.setText(getString(R.string.point_buy_output,
+                roll_stat(),roll_stat(),roll_stat(),
+                roll_stat(),roll_stat(),roll_stat()));
+    }
+
+    // simulate rolling 4 six sided dice, then remove the lowest roll
+    public String roll_stat() {
+        Random rand = new Random();
+        int[] rolls = new int[4];
+        int total = 0;
+        int droped_roll = 6;
+
+        for(int i = 0; i<= 3; i++){
+            rolls[i] = rand.nextInt(6) + 1;;
+        }
+
+        for(int i = 0; i<= 3; i++){
+            droped_roll = Math.min(droped_roll, rolls[i]);
+            total += rolls[i];
+        }
+
+        return Integer.toString(total - droped_roll);
+    }
+
+}
--- app/src/main/java/com/example/zeilinger/test/MainActivity.java
+++ app/src/main/java/com/example/zeilinger/test/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.zeilinger.test;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -17,11 +18,7 @@ public class MainActivity extends AppCompatActivity {
     EditText sidesPerDice;
     EditText numberOfDice;
 
-
-
-
-
-    //navigation between instances
+    //navigation blocks
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -32,7 +29,10 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_points);
+                    Intent myintent = new Intent(MainActivity.this,Main2Activity.class);
+                    startActivity(myintent);
+
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -44,22 +44,19 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 
-    public void sendMessage(View view) {
 
-
+    //activity logic blocks
+    public void rolldice(View view) {
         sidesPerDice = (EditText)findViewById(R.id.sidesPerDice);
         numberOfDice = (EditText)findViewById(R.id.numberOfDice);
         if(sidesPerDice.getText().toString().isEmpty()||numberOfDice.getText().toString().isEmpty()){
@@ -75,16 +72,10 @@ public class MainActivity extends AppCompatActivity {
             numberOfDice_int--;
         }
 
-        //Toast.makeText(MainActivity.this,
-        //        Integer.toString(call_rng(50)), Toast.LENGTH_SHORT).show();
-
-        //Toast.makeText(MainActivity.this,
-        //        sidesPerDice.getText().toString(), Toast.LENGTH_SHORT).show();
-
         Toast.makeText(MainActivity.this,
                 Integer.toString(total), Toast.LENGTH_SHORT).show();
-    }
 
+    }
 
 
     private int call_rng(int max_value){
